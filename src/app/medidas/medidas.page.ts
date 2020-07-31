0441d3827044e015c0987e0902b18081f05a24f1
Ramon Figuera <refm.130995@gmail.com>
2020-07-30 22:14:41 -0400

    Alerta

--- src/app/medidas/medidas.page.ts
+++ src/app/medidas/medidas.page.ts
@@ -203,6 +203,33 @@ phoneFormatView(num:any, input:string){  //formatea la vista del número
      });
   }
 
+  async alerta() {
+    const alert = await this.alertCtrl.create({
+      header: 'Si no lo haces ahora, puedes hacerlo luego en el apartado de "medidas" dentro de tu perfil.  No pierdas la posibilidad de comparar tus medidas posteriormente, cada 6 a 8 semanas te vamos a recordar para poder comparar tu progreso y cuantificarlo.',
+      cssClass: 'customMensaje1',
+      buttons: [
+        {
+          text: 'Cancelar',
+          role: 'cancel',
+          cssClass: 'cancelButton',
+          handler: (blah) => {
+           
+          }
+        }, {
+          text: 'Saltar',
+          cssClass: 'confirmButton',
+          handler: () => {
+            // mensaje confirmacion
+           
+          }
+        }
+      ]
+
+    });
+
+    await alert.present();
+  }
+
   seleccionarFuente() {
     return new Promise(async resolve => {
 
