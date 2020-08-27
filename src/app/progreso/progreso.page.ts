cded9bda043d8776122344dd19a197a4c64a513e
bryan <bryanjose84@hotmail.com>
2020-08-27 00:59:08 -0400

    fsfs

--- src/app/progreso/progreso.page.ts
+++ src/app/progreso/progreso.page.ts
@@ -14,7 +14,45 @@ export class ProgresoPage implements OnInit {
   clase:string = 'perfil'
   valor: any;
   valor2: any;
-
+  fechas:any
+  medidas_ante = {
+    min_waist: null,
+    max_waist:null,
+    hip:null,
+    neck:null,
+    right_thigh:null,
+    left_thigh:null,
+    right_arm:null,
+    left_arm:null,
+    right_arm_flexed:null,
+    left_arm_flexed:null,
+    right_calf:null,
+    left_calf:null,
+    torax:null,
+    waist_hip:null,
+    profile_photo:null,
+    front_photo:null,
+    back_photo:null,
+  }
+  medidas_despues = {
+    min_waist: null,
+    max_waist:null,
+    hip:null,
+    neck:null,
+    right_thigh:null,
+    left_thigh:null,
+    right_arm:null,
+    left_arm:null,
+    right_arm_flexed:null,
+    left_arm_flexed:null,
+    right_calf:null,
+    left_calf:null,
+    torax:null,
+    waist_hip:null,
+    profile_photo:null,
+    front_photo:null,
+    back_photo:null,
+  }
 
 
   constructor( private ruta: NavController,
@@ -23,7 +61,7 @@ export class ProgresoPage implements OnInit {
               private utilities: MensajesService) { }
 
   ngOnInit() {
-    // this.getResume()
+    this.getResume()
   }
 
 
@@ -34,13 +72,14 @@ export class ProgresoPage implements OnInit {
 
   async getResume(){
       this.presentLoading() 
-       const valor = await this.service.historyMeasures()
+       const valor:any = await this.service.historyMeasures()
        if(valor == false ){
         this.loadingController.dismiss() 
        this.utilities.notificacionUsuario('Disculpe, Ha ocurrido un error', 'danger')
        }else{
         this.loadingController.dismiss() 
-          console.log(valor)
+         console.log(valor)
+         this.fechas = valor
        }
          
    }
@@ -48,6 +87,7 @@ export class ProgresoPage implements OnInit {
    async presentLoading() {
     const loading = await this.loadingController.create({
       message: 'Por favor espere...',
+      cssClass: 'my-loading',
     });
     await loading.present();
   }
@@ -57,19 +97,71 @@ export class ProgresoPage implements OnInit {
   
   segmentChanged(valor){
     this.clase = valor.target.value
+    this.comparar = valor.target.value
+    this.valor = null
+    this.valor2 = null
+    for (var key in this.medidas_ante ) {
+      this.medidas_ante[key] = null;
+    }
 
-   this.comparar = valor.target.value
+    for (var key in this.medidas_despues ) {
+      this.medidas_despues[key] = null;
+    }
+
   }
 
 
   desde(valor){
     console.log(valor.target.value)
     this.valor = valor.target.value;
+    this.buscador(this.valor,"ante")
   }
 
   hasta(valor){
     console.log(valor.target.value)
     this.valor2 = valor.target.value;
+    this.buscador(this.valor2,"despues")
+  }
+
+  buscador(valor, filtrar){
+    const resultado = this.fechas.find( elemento => elemento.id === parseInt(valor) );
+    console.log(resultado);
+    if(filtrar === "ante"){
+      this.medidas_ante.min_waist = resultado.min_waist,
+      this.medidas_ante.max_waist = resultado.max_waist,
+      this.medidas_ante.hip = resultado.hip,
+      this.medidas_ante.neck = resultado.neck,
+      this.medidas_ante.right_thigh = resultado.right_thigh,
+      this.medidas_ante.left_thigh = resultado.left_thigh,
+      this.medidas_ante.right_arm = resultado.right_arm,
+      this.medidas_ante.left_arm = resultado.left_arm,
+      this.medidas_ante.right_arm_flexed = resultado.right_arm_flexed,
+      this.medidas_ante.left_arm_flexed = resultado.left_arm_flexed,
+      this.medidas_ante.right_calf = resultado.right_calf,
+      this.medidas_ante.left_calf = resultado.left_calf,
+      this.medidas_ante.torax = resultado.torax,
+      this.medidas_ante.profile_photo = resultado.profile_photo,
+      this.medidas_ante.front_photo = resultado.front_photo,
+      this.medidas_ante.back_photo = resultado.back_photo
+    }else{
+      this.medidas_despues.min_waist = resultado.min_waist,
+      this.medidas_despues.max_waist = resultado.max_waist,
+      this.medidas_despues.hip = resultado.hip,
+      this.medidas_despues.neck = resultado.neck,
+      this.medidas_despues.right_thigh = resultado.right_thigh,
+      this.medidas_despues.left_thigh = resultado.left_thigh,
+      this.medidas_despues.right_arm_flexed = resultado.right_arm_flexed,
+      this.medidas_despues.right_arm = resultado.right_arm,
+      this.medidas_despues.left_arm = resultado.left_arm,
+      this.medidas_despues.left_arm_flexed = resultado.left_arm_flexed,
+      this.medidas_despues.right_calf = resultado.right_calf,
+      this.medidas_despues.left_calf = resultado.left_calf,
+      this.medidas_despues.torax = resultado.torax,
+      this.medidas_despues.profile_photo = resultado.profile_photo,
+      this.medidas_despues.front_photo = resultado.front_photo,
+      this.medidas_despues.back_photo = resultado.back_photo
+    }
+
   }
 
 
