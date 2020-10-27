5d2c621e590542b5537dec2e72b292960dccaf1e
bryan <bryanjose84@hotmail.com>
2020-10-26 23:05:34 -0400

    2FS

---
+++ src/app/services/tienda.service.ts
@@ -0,0 +1,33 @@
+import { HttpClient, HttpHeaders } from '@angular/common/http';
+import { Injectable } from '@angular/core';
+import { ApiFitechService } from './api-fitech.service';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class TiendaService {
+
+  constructor(private http: HttpClient, private service: ApiFitechService) { }
+
+    // listado de productos
+    getProducts(){
+      return new Promise( async (resolve, reject)  => {
+        const headers = new HttpHeaders({
+          'Authorization': 'Bearer ' + await this.service.cargarToken(),
+          'Content-Type':'application/json',
+        })
+          // si no se envia un dato no  funciona la ruta
+        const data = {
+          valor : "ignorar"
+        } 
+
+        this.http.post(`http://fittech247.com/fittech/api/auth/products`,data,{headers})
+            .subscribe(resp=>{
+              resolve(resp['Productos'])
+            },err=>{
+              reject(false)
+            })
+        })
+    }
+
+}
--- src/app/tienda-productos/tienda-productos.page.ts
+++ src/app/tienda-productos/tienda-productos.page.ts
@@ -1,4 +1,8 @@
 import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
+import { LoadingController } from '@ionic/angular';
+import { MensajesService } from '../services/mensajes.service';
+import { TiendaService } from '../services/tienda.service';
 
 @Component({
   selector: 'app-tienda-productos',
@@ -6,10 +10,41 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./tienda-productos.page.scss'],
 })
 export class TiendaProductosPage implements OnInit {
+  productos:any = []
+  buscador:any = []
+  name:any
+  searchTerm: any;
 
-  constructor() { }
+  constructor( private service: TiendaService,
+              private route: ActivatedRoute,
+              public loadingController: LoadingController,
+              private utilities: MensajesService,) {
+                this.route.queryParams.subscribe(params => {
+                  this.productos = JSON.parse(params["shop"]);
+                  this.name = this.productos.name
+                  this.productos = this.productos.product
+                  this.buscador = this.productos
+                  });
+               }
 
   ngOnInit() {
+  }
+
+  search(){
+     if(!this.searchTerm){
+        this.productos = this.buscador
+     }else{
+       this.productos = this.filterItems()
+     }
+  }
+
+  filterItems(){
+    return this.productos.filter((item) => {
+      console.log(item);
+      return (
+        item.name.toLowerCase().indexOf(this.searchTerm.toLowerCase()) > -1 
+      );
+    });
   }
 
 }
