747f37f01f25a326a51a7bafb242d1834f89e4e5
refm130995 <refm.130995@gmail.com>
2020-12-17 15:32:06 -0400

    ----->Modulo de notificaciones.

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,29 +1,29 @@
-import { NgModule } from '@angular/core';
-import { BrowserModule } from '@angular/platform-browser';
-import { RouteReuseStrategy } from '@angular/router';
+import { NgModule } from "@angular/core";
+import { BrowserModule } from "@angular/platform-browser";
+import { RouteReuseStrategy } from "@angular/router";
 
-import { GooglePlus } from '@ionic-native/google-plus/ngx';
+import { GooglePlus } from "@ionic-native/google-plus/ngx";
 
-import { AngularFireModule } from '@angular/fire';
-import { AngularFireAuthModule } from '@angular/fire/auth';
-import { Facebook, FacebookLoginResponse } from '@ionic-native/facebook/ngx';
+import { AngularFireModule } from "@angular/fire";
+import { AngularFireAuthModule } from "@angular/fire/auth";
+import { Facebook, FacebookLoginResponse } from "@ionic-native/facebook/ngx";
 
-import { IonicStorageModule } from '@ionic/storage';
+import { IonicStorageModule } from "@ionic/storage";
 
+import { ComponentsModule } from "./components/components.module";
+import { IonicModule, IonicRouteStrategy } from "@ionic/angular";
+import { SplashScreen } from "@ionic-native/splash-screen/ngx";
+import { StatusBar } from "@ionic-native/status-bar/ngx";
 
-import { ComponentsModule } from './components/components.module';
-import { IonicModule, IonicRouteStrategy } from '@ionic/angular';
-import { SplashScreen } from '@ionic-native/splash-screen/ngx';
-import { StatusBar } from '@ionic-native/status-bar/ngx';
-
-import { AppComponent } from './app.component';
-import { AppRoutingModule } from './app-routing.module';
-import { HttpClientModule } from '@angular/common/http';
-import { FileTransfer } from '@ionic-native/file-transfer/ngx';
-import { File } from '@ionic-native/file/ngx';
-import { FormsModule, ReactiveFormsModule } from '@angular/forms';
-import { Camera, CameraOptions } from '@ionic-native/camera/ngx';
-import {WebView} from '@ionic-native/ionic-webview/ngx';
+import { AppComponent } from "./app.component";
+import { AppRoutingModule } from "./app-routing.module";
+import { HttpClientModule } from "@angular/common/http";
+import { FileTransfer } from "@ionic-native/file-transfer/ngx";
+import { File } from "@ionic-native/file/ngx";
+import { FormsModule, ReactiveFormsModule } from "@angular/forms";
+import { Camera, CameraOptions } from "@ionic-native/camera/ngx";
+import { WebView } from "@ionic-native/ionic-webview/ngx";
+import { OneSignal } from "@ionic-native/onesignal/ngx";
 
 var firebaseConfig = {
   apiKey: "AIzaSyC05iYDOCHztv5ujJBlVY05SO7WURpGtc0",
@@ -33,16 +33,15 @@ var firebaseConfig = {
   storageBucket: "login-32a05.appspot.com",
   messagingSenderId: "852806702714",
   appId: "1:852806702714:android:b31632fd4b3cb56023cdc3",
-  measurementId: "G-WK88CRWK4F"
-}
-
+  measurementId: "G-WK88CRWK4F",
+};
 
 @NgModule({
   declarations: [AppComponent],
   entryComponents: [],
   imports: [
-    BrowserModule, 
-    IonicModule.forRoot({hardwareBackButton: false}), 
+    BrowserModule,
+    IonicModule.forRoot({ hardwareBackButton: false }),
     AppRoutingModule,
     HttpClientModule,
     ComponentsModule,
@@ -61,8 +60,9 @@ var firebaseConfig = {
     SplashScreen,
     Camera,
     WebView,
-    { provide: RouteReuseStrategy, useClass: IonicRouteStrategy }
+    { provide: RouteReuseStrategy, useClass: IonicRouteStrategy },
+    OneSignal,
   ],
-  bootstrap: [AppComponent]
+  bootstrap: [AppComponent],
 })
 export class AppModule {}
