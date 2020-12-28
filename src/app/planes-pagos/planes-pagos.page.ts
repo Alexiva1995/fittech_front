1985f7254f560247dd635c32e33a7df52ab3cb6e
bryan <bryanjose84@hotmail.com>
2020-12-27 20:25:16 -0400

    FS

--- src/app/planes-pagos/planes-pagos.page.ts
+++ src/app/planes-pagos/planes-pagos.page.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import { LoadingController } from '@ionic/angular';
+import { MensajesService } from '../services/mensajes.service';
+import { NutricionService } from '../services/nutricion.service';
 
 @Component({
   selector: 'app-planes-pagos',
@@ -6,10 +9,33 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./planes-pagos.page.scss'],
 })
 export class PlanesPagosPage implements OnInit {
+  dato:any
 
-  constructor() { }
+  constructor(private service: NutricionService,
+    private utilities: MensajesService,
+    public loadingController: LoadingController,) { }
 
   ngOnInit() {
+    this.getPremium()
+  }
+
+  async getPremium(){
+    this.presentLoading()
+    const valor = await this.service.GetPrice()
+    this.loadingController.dismiss()
+      if(valor == false ){
+        this.utilities.notificacionUsuario("Ocurrio un error, revise su conexión","danger")
+      }else{
+        this.dato = valor
+      }
+  }
+
+
+  async presentLoading() {
+    const loading = await this.loadingController.create({
+      message: 'Por favor espere...',
+    });
+    await loading.present();
   }
 
 }
